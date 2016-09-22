3327cffa55d7e94a3f8be3ba06192efc1d8dc68e
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2016-09-22 23:32:56 +0300

    Added week, month, project normProgress

--- src/WorkManager/Services/Norms/NormService.cs
+++ src/WorkManager/Services/Norms/NormService.cs
@@ -7,6 +7,8 @@ using WorkManager.Data;
 using WorkManager.Models;
 using WorkManager.Models.Norms;
 using WorkManager.Services.Timers;
+using WorkManager.Helpers;
+using WorkManager.Services.Projects;
 
 namespace WorkManager.Services.Norms
 {
@@ -14,11 +16,13 @@ namespace WorkManager.Services.Norms
     {
         private readonly ITimerService _timers;
         private readonly ApplicationDbContext _context;
+        private readonly IProjectsService _projects;
 
-        public NormService(ITimerService timers, ApplicationDbContext context)
+        public NormService(ITimerService timers, ApplicationDbContext context, IProjectsService projects)
         {
             _timers = timers;
             _context = context;
+            _projects = projects;
         }
 
         public async Task<Norm> GetNormAsync(Project project)
@@ -36,16 +40,20 @@ namespace WorkManager.Services.Norms
                     var duration = _timers.GetDuration(_timers.GetTimersInInterval(project, date.Date, date.Date.AddDays(1)));
                     return new NormProgress(norm, norm.Goal, duration);
                 case NormType.Week:
-                    throw new NotImplementedException();
-                    break;
+                    var startOfWeek = DateHelpers.GetStartOfWeek(date, _projects.GetCulture(project));
+                    // Get [(start of week)..date] duration
+                    var curWeekDuration = _timers.GetDuration(_timers.GetTimersInInterval(project, startOfWeek, date));
+                    return new NormProgress(norm, norm.Goal, curWeekDuration);
                 case NormType.Month:
-                    throw new NotImplementedException();
-                    break;
+                    var startOfMonth = DateHelpers.GetStartOfMonth(date);
+                    // Get [(start of month)..date] duration
+                    var curMonthDuration = _timers.GetDuration(_timers.GetTimersInInterval(project, startOfMonth, date));
+                    return new NormProgress(norm, norm.Goal, curMonthDuration);
                 case NormType.Project:
                     var timers = _timers.GetDuration(_timers.GetTimersInInterval(project, null, date));
                     return new NormProgress(norm, norm.Goal, timers);
                 case NormType.None:
-                    return null;
+                    return new NormProgress(norm, new TimeSpan(0), new TimeSpan(0));
             }
             throw new ArgumentException();
         }
